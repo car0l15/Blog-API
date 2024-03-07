76c05380ecb0e42fd15f73d3427e0b28eb36b3ff
car0l15 <carol.nepomuceno95@gmail.com>
2024-03-07 13:41:51 -0300

    feat: implementando Context e Repository

---
+++ src/Repository/BlogContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using projeto_final.Models;
+using System;
+
+namespace projeto_final.Repository;
+
+public class TryBlogContext : DbContext, ITryBlogContext
+{
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<User> Users { get; set; }
+    public TryBlogContext(DbContextOptions<TryBlogContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var server = Environment.GetEnvironmentVariable("DB_SERVER");
+            var database = Environment.GetEnvironmentVariable("DB_DATABASE");
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            var trustServerCertificate = Environment.GetEnvironmentVariable("DB_TRUST_SERVER_CERTIFICATE");
+
+            optionsBuilder.UseSqlServer($@"
+                Server={server};
+                Database={database};
+                User={user};
+                Password={password};
+                TrustServerCertificate={trustServerCertificate};
+            ").UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);;
+        }
+    }
+}
